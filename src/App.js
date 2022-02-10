05a5e799ebe0e9726d5ae6588de28fdb13d092b7
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-10 15:51:12 +0530

    NavLink classname added dynamically

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 function App() {
-  return(
+  const setClassName = (state) => {
+    return state ? "btn btn-secondary me-3" : "btn btn-primary me-3";
+  };
+  return (
     <>
-    
-    <header className="header">suggest me course</header>
-    <h1 className="heading">select your interest:</h1>
-    <NavLink to="/">FrontEnd</NavLink>
-    <NavLink to="/">BackEnd</NavLink>
-    <NavLink to="/">Full Stack</NavLink>
+      <header className="header">suggest me course</header>
+      <h1 className="heading">select your interest:</h1>
+      <NavLink
+        className={({ isActive }) => setClassName(isActive)}
+        to="/frontend"
+      >
+        FrontEnd
+      </NavLink>
+      <NavLink className={({ isActive }) => setClassName(isActive)} to="/backend">
+        BackEnd
+      </NavLink>
+      <NavLink className={({ isActive }) => setClassName(isActive)} to="/fullstack">
+        Full Stack
+      </NavLink>
+
+      <Outlet />
     </>
   );
 }
