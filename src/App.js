014fa652ae169a42c8e011b12abc1c143110bb54
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-10 15:24:09 +0530

    Markup and NavLink Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import "./App.css";
+import { NavLink } from "react-router-dom";
 
 function App() {
-  return;
+  return(
+    <>
+    
+    <header className="header">suggest me course</header>
+    <h1 className="heading">select your interest:</h1>
+    <NavLink to="/">FrontEnd</NavLink>
+    <NavLink to="/">BackEnd</NavLink>
+    <NavLink to="/">Full Stack</NavLink>
+    </>
+  );
 }
 
 export default App;
