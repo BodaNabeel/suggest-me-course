675c6bc1ce5716440e3e727c8c203f9fe1341744
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-11 02:32:04 +0530

    added content of backend courses

--- src/components/stack/BackEnd.jsx
+++ src/components/stack/BackEnd.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React from "react";
 
 export default function BackEnd() {
   return (
-    <div>BackEnd</div>
-  )
+    <>
+      <div className="card">
+        <img
+          src="https://img-c.udemycdn.com/course/240x135/1672410_9ff1_5.jpg"
+          alt=""
+          className="card-img"
+        />
+        <p className="card-title">
+          Node.js, Express, MongoDB & More: The Complete Bootcamp 2022
+        </p>
+        <button className="card-btn btn-primary">check out</button>
+      </div>
+      <div className="card">
+        <img
+          src="https://img-c.udemycdn.com/course/240x135/1879018_95b6_3.jpg"
+          alt=""
+          className="card-img"
+        />
+        <p className="card-title">
+          NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno){" "}
+        </p>
+        <button className="card-btn btn-primary">check out</button>
+      </div>
+      <div className="card">
+        <img
+          src="https://img-c.udemycdn.com/course/240x135/684824_3626_3.jpg"
+          alt=""
+          className="card-img"
+        />
+        <p className="card-title">
+          Spring Framework Master Class - Java Spring the Modern Way{" "}
+        </p>
+        <button className="card-btn btn-primary">check out</button>
+      </div>
+      <div className="card">
+        <img
+          src="https://img-c.udemycdn.com/course/240x135/1699208_604f_3.jpg"
+          alt=""
+          className="card-img"
+        />
+        <p className="card-title">
+          The Flask Mega-Tutorial (Python Web Development)
+        </p>
+        <button className="card-btn btn-primary">check out</button>
+      </div>
+    </>
+  );
 }
