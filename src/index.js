022836cc1ca38f258def5439e87f31b48009256a
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-11 00:56:59 +0530

    sample elements replaced w/ real elements

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router ,Route, Routes } from 'react-router-dom';
-const Sample = () => {
-  return "HELLO"
-}
+import FrontEnd from './components/stack/FrontEnd';
+import BackEnd from './components/stack/BackEnd';
+import FullStack from './components/stack/FullStack';
+
 ReactDOM.render(
   <Router>
     <Routes>
       <Route path='/' element={<App/>}>
-        <Route path="frontend" element={<Sample/>}/>
-        <Route path="backend" element={<Sample/>}/>
-        <Route path="fullstack" element={<Sample/>}/>
+        <Route path="frontend" element={<FrontEnd/>}/>
+        <Route path="backend" element={<BackEnd/>}/>
+        <Route path="fullstack" element={<FullStack/>}/>
       </Route>
     </Routes>
   </Router>,
