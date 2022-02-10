169ad23224b791efa1c3b2392f712c540a8631c9
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-11 02:37:26 +0530

    added content of fullstack courses

--- src/components/stack/FullStack.jsx
+++ src/components/stack/FullStack.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import React from "react";
 
 export default function FullStack() {
   return (
-    <div>FullStack</div>
-  )
+    <>
+      <div className="card">
+        <img
+          src="https://img-b.udemycdn.com/course/240x135/2640372_5b44_5.jpg?secure=wDfQpdLWRAfsMOZCAav5Dw%3D%3D%2C1644566660"
+          alt=""
+          className="card-img"
+        />
+        <p className="card-title">
+          React, NodeJS, Express & MongoDB - The MERN Fullstack Guide
+        </p>
+        <button className="card-btn btn-primary">check out</button>
+      </div>
+      <div className="card">
+        <img
+          src="https://img-c.udemycdn.com/course/240x135/3655840_1c3c.jpg"
+          alt=""
+          className="card-img"
+        />
+        <p className="card-title">
+        The Ultimate 2022 Fullstack Web Development Bootcamp
+        </p>
+        <button className="card-btn btn-primary">check out</button>
+      </div>
+      <div className="card">
+        <img
+          src="https://img-c.udemycdn.com/course/240x135/1646980_23f7_2.jpg"
+          alt=""
+          className="card-img"
+        />
+        <p className="card-title">
+        MERN Stack Front To Back: Full Stack React, Redux & Node.js
+        </p>
+        <button className="card-btn btn-primary">check out</button>
+      </div>
+      <div className="card">
+        <img
+         src="https://img-c.udemycdn.com/course/240x135/822444_a6db.jpg"
+          alt=""
+          className="card-img"
+        />
+        <p className="card-title">
+        Python and Django Full Stack Web Developer Bootcamp
+        </p>
+        <button className="card-btn btn-primary">check out</button>
+      </div>
+      <div className="card">
+        <img
+         src="https://img-b.udemycdn.com/course/240x135/1042110_ffc3_4.jpg?secure=EkF9z2_XaXJoeaQGLaokSg%3D%3D%2C1644564995"
+          alt=""
+          className="card-img"
+        />
+        <p className="card-title">
+        Beginner Full Stack Web Development: HTML, CSS, React & Node
+        </p>
+        <button className="card-btn btn-primary">check out</button>
+      </div>
+    </>
+  );
 }
