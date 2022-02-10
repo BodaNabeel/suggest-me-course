78c0594dee302810a46594d03fa79fb76ea7aa9f
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-11 02:31:53 +0530

    removed few courses

--- src/components/stack/FrontEnd.jsx
+++ src/components/stack/FrontEnd.jsx
@@ -27,28 +27,6 @@ export default function FrontEnd() {
       </div>
       <div className="card">
         <img
-          src="https://img-c.udemycdn.com/course/240x135/851712_fc61_6.jpg"
-          alt=""
-          className="card-img"
-        />
-        <p className="card-title">
-          The Complete JavaScript Course 2022: From Zero to Expert!
-        </p>
-        <button className="card-btn btn-primary">check out</button>
-      </div>
-      <div className="card">
-        <img
-          src="https://img-c.udemycdn.com/course/240x135/2508942_11d3_3.jpg"
-          alt=""
-          className="card-img"
-        />
-        <p className="card-title">
-          JavaScript - The Complete Guide 2022 (Beginner + Advanced)
-        </p>
-        <button className="card-btn btn-primary">check out</button>
-      </div>
-      <div className="card">
-        <img
           src="https://img-c.udemycdn.com/course/240x135/1362070_b9a1_2.jpg"
           alt=""
           className="card-img"
@@ -88,17 +66,6 @@ export default function FrontEnd() {
         />
         <p className="card-title">
         Vue - The Complete Guide (incl. Router & Composition API)        </p>
-        <button className="card-btn btn-primary">check out</button>
-      </div>
-      <div className="card">
-        <img
-          src="https://img-c.udemycdn.com/course/240x135/756150_c033_2.jpg"
-          alt=""
-          className="card-img"
-        />
-        <p className="card-title">
-        Angular - The Complete Guide (2022 Edition)
-        </p>
         <button className="card-btn btn-primary">check out</button>
       </div>
       
