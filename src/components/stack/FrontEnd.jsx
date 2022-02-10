094ea356e7e7d4e6f39e4782d9c211c47b72cf49
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-11 00:57:33 +0530

    FrontEnd component added

---
+++ src/components/stack/FrontEnd.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function FrontEnd() {
+  return (
+    <>
+      <div className="card">
+        <img
+          src="https://img-c.udemycdn.com/course/240x135/437398_46c3_10.jpg"
+          alt=""
+          className="card-img"
+        />
+        <p className="card-title">HTML and CSS</p>
+        <button className="btn btn-primary">check out</button>
+      </div>
+    </>
+  );
+}
