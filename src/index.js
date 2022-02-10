c9c504f45dee2e065d45a8ff5d94de4c06572e4e
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-10 15:51:33 +0530

    paths added with sample elements for now

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router ,Route, Routes } from 'react-router-dom';
-
+const Sample = () => {
+  return "HELLO"
+}
 ReactDOM.render(
   <Router>
     <Routes>
-      <Route path='/' element={<App/>}/>
+      <Route path='/' element={<App/>}>
+        <Route path="frontend" element={<Sample/>}/>
+        <Route path="backend" element={<Sample/>}/>
+        <Route path="fullstack" element={<Sample/>}/>
+      </Route>
     </Routes>
   </Router>,
   document.getElementById('root')
 );
+
