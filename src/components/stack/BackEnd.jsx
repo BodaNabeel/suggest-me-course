653158f5bcd5a069d244e4983ee402bb01a96126
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-11 12:48:15 +0530

    replaced hardcoded markup with dynamic markup

--- src/components/stack/BackEnd.jsx
+++ src/components/stack/BackEnd.jsx
@@ -1,52 +1,39 @@
 import React from "react";
 
 export default function BackEnd() {
+  const backendDetail = [
+    {
+      cardSrc: "https://img-c.udemycdn.com/course/240x135/1672410_9ff1_5.jpg",
+      cardTitle: "Node.js, Express, MongoDB & More: The Complete Bootcamp 2022",
+      cardPath: "",
+    },
+    {
+      cardSrc: "https://img-c.udemycdn.com/course/240x135/1879018_95b6_3.jpg",
+      cardTitle: "NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)",
+      cardPath: "",
+    },
+    {
+      cardSrc: "https://img-c.udemycdn.com/course/240x135/684824_3626_3.jpg",
+      cardTitle: "Spring Framework Master Class - Java Spring the Modern Way",
+      cardPath: "",
+    },
+    {
+      cardSrc: "https://img-c.udemycdn.com/course/240x135/1699208_604f_3.jpg",
+      cardTitle: "The Flask Mega-Tutorial (Python Web Development)",
+      cardPath: "",
+    },
+  ];
   return (
     <>
-      <div className="card">
-        <img
-          src="https://img-c.udemycdn.com/course/240x135/1672410_9ff1_5.jpg"
-          alt=""
-          className="card-img"
-        />
-        <p className="card-title">
-          Node.js, Express, MongoDB & More: The Complete Bootcamp 2022
-        </p>
-        <button className="card-btn btn-primary">check out</button>
-      </div>
-      <div className="card">
-        <img
-          src="https://img-c.udemycdn.com/course/240x135/1879018_95b6_3.jpg"
-          alt=""
-          className="card-img"
-        />
-        <p className="card-title">
-          NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno){" "}
-        </p>
-        <button className="card-btn btn-primary">check out</button>
-      </div>
-      <div className="card">
-        <img
-          src="https://img-c.udemycdn.com/course/240x135/684824_3626_3.jpg"
-          alt=""
-          className="card-img"
-        />
-        <p className="card-title">
-          Spring Framework Master Class - Java Spring the Modern Way{" "}
-        </p>
-        <button className="card-btn btn-primary">check out</button>
-      </div>
-      <div className="card">
-        <img
-          src="https://img-c.udemycdn.com/course/240x135/1699208_604f_3.jpg"
-          alt=""
-          className="card-img"
-        />
-        <p className="card-title">
-          The Flask Mega-Tutorial (Python Web Development)
-        </p>
-        <button className="card-btn btn-primary">check out</button>
-      </div>
+      {backendDetail.map((detail) => {
+        return (
+          <div className="card">
+            <img src={detail.cardSrc} alt="" className="card-img" />
+            <p className="card-title">{detail.cardTitle}</p>
+            <button className="card-btn btn-primary">check out</button>
+          </div>
+        );
+      })}
     </>
   );
 }
--- src/components/stack/FullStack.jsx
+++ src/components/stack/FullStack.jsx
@@ -1,63 +1,48 @@
 import React from "react";
 
 export default function FullStack() {
+  const fullstackDetail = [
+    {
+      cardSrc:
+        "https://img-b.udemycdn.com/course/240x135/2640372_5b44_5.jpg?secure=wDfQpdLWRAfsMOZCAav5Dw%3D%3D%2C1644566660",
+      cardTitle: "React, NodeJS, Express & MongoDB - The MERN Fullstack Guide",
+      cardPath: "",
+    },
+    {
+      cardSrc: "https://img-c.udemycdn.com/course/240x135/3655840_1c3c.jpg",
+      cardTitle: "The Ultimate 2022 Fullstack Web Development Bootcamp",
+      cardPath: "",
+    },
+    {
+      cardSrc: "https://img-c.udemycdn.com/course/240x135/1646980_23f7_2.jpg",
+      cardTitle:
+        "  MERN Stack Front To Back: Full Stack React, Redux & Node.js",
+      cardPath: "",
+    },
+    {
+      cardSrc: "https://img-c.udemycdn.com/course/240x135/822444_a6db.jpg",
+      cardTitle: "Python and Django Full Stack Web Developer Bootcamp",
+      cardPath: "",
+    },
+    {
+      cardSrc:
+        "https://img-b.udemycdn.com/course/240x135/1042110_ffc3_4.jpg?secure=EkF9z2_XaXJoeaQGLaokSg%3D%3D%2C1644564995",
+      cardTitle:
+        " Beginner Full Stack Web Development: HTML, CSS, React & Node",
+      cardPath: "",
+    },
+  ];
   return (
     <>
-      <div className="card">
-        <img
-          src="https://img-b.udemycdn.com/course/240x135/2640372_5b44_5.jpg?secure=wDfQpdLWRAfsMOZCAav5Dw%3D%3D%2C1644566660"
-          alt=""
-          className="card-img"
-        />
-        <p className="card-title">
-          React, NodeJS, Express & MongoDB - The MERN Fullstack Guide
-        </p>
-        <button className="card-btn btn-primary">check out</button>
-      </div>
-      <div className="card">
-        <img
-          src="https://img-c.udemycdn.com/course/240x135/3655840_1c3c.jpg"
-          alt=""
-          className="card-img"
-        />
-        <p className="card-title">
-        The Ultimate 2022 Fullstack Web Development Bootcamp
-        </p>
-        <button className="card-btn btn-primary">check out</button>
-      </div>
-      <div className="card">
-        <img
-          src="https://img-c.udemycdn.com/course/240x135/1646980_23f7_2.jpg"
-          alt=""
-          className="card-img"
-        />
-        <p className="card-title">
-        MERN Stack Front To Back: Full Stack React, Redux & Node.js
-        </p>
-        <button className="card-btn btn-primary">check out</button>
-      </div>
-      <div className="card">
-        <img
-         src="https://img-c.udemycdn.com/course/240x135/822444_a6db.jpg"
-          alt=""
-          className="card-img"
-        />
-        <p className="card-title">
-        Python and Django Full Stack Web Developer Bootcamp
-        </p>
-        <button className="card-btn btn-primary">check out</button>
-      </div>
-      <div className="card">
-        <img
-         src="https://img-b.udemycdn.com/course/240x135/1042110_ffc3_4.jpg?secure=EkF9z2_XaXJoeaQGLaokSg%3D%3D%2C1644564995"
-          alt=""
-          className="card-img"
-        />
-        <p className="card-title">
-        Beginner Full Stack Web Development: HTML, CSS, React & Node
-        </p>
-        <button className="card-btn btn-primary">check out</button>
-      </div>
+      {fullstackDetail.map((detail) => {
+        return (
+          <div className="card">
+            <img src={detail.cardSrc} alt="" className="card-img" />
+            <p className="card-title">{detail.cardTitle}</p>
+            <button className="card-btn btn-primary">check out</button>
+          </div>
+        );
+      })}
     </>
   );
 }
