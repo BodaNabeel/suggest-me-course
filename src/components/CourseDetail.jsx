5c7bbb882423de0b3486cc07ee96055314658ceb
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-12 18:25:53 +0530

    replaced ul & li tag with div

--- src/components/CourseDetail.jsx
+++ src/components/CourseDetail.jsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import { FaCheck,FaDotCircle } from "react-icons/fa";
 import { v4 as uuidv4 } from "uuid";
 
 export default function CourseDetail({ database }) {
@@ -16,41 +17,44 @@ export default function CourseDetail({ database }) {
               className="container-top_img"
             />
             <h1 className="container-top_title">{database.title}</h1>
+            {/* TODO: make tutor name dynamic */}
             <p className="container-top_author">Created by: {database.tutor}</p>
           </div>
 
-          <div className="learn">
-            <ul className="learn-list list-1">
-              {database.learnPartOne.map((learn) => {
-                return (
-                  <li className="learn-item" key={uuidv4()}>
-                    {learn}
-                  </li>
-                );
-              })}
-            </ul>
-            <ul className="learn-list list-2">
-              {database.learnPartTwo.map((learn) => {
-                return (
-                  <li className="learn-item" key={uuidv4()}>
-                    {learn}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-
-          <div className="includes">
-            <p className="includes-title">Incudes</p>
-            <ul className="includes-list" key={database.id}>
+          <div className="content">
+            <div className="learn">
+              <div key={uuidv4()} className="learn-list list-1">
+                {database.learnPartOne.map((learn) => {
+                  return (
+                    <div className="learn-item" key={uuidv4()}>
+                      <span className="learn-icon">{<FaCheck />}</span>
+                      {learn}
+                    </div>
+                  );
+                })}
+              </div>
+              <div key={uuidv4()} className="learn-list list-2">
+                {database.learnPartTwo.map((learn) => {
+                  return (
+                    <div className="learn-item" key={uuidv4()}>
+                      <span className="learn-icon">{<FaCheck />}</span>
+                      {learn}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="includes">
+              <h3 className="includes-title">This course includes</h3>
               {database.includes.map((includes) => {
                 return (
-                  <li className="includes-item" key={uuidv4()}>
+                  <div className="includes-item" key={uuidv4()}>
+                    <span className="includes-icon"><FaDotCircle/></span>
                     {includes}
-                  </li>
+                  </div>
                 );
               })}
-            </ul>
+            </div>
           </div>
         </div>
       );
