6fe278e5217ef6a5e0cb9d6f87f9e5fe0adba232
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-12 15:49:12 +0530

    markup added of CourseDetail

--- src/components/CourseDetail.jsx
+++ src/components/CourseDetail.jsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 export default function CourseDetail({ database }) {
   const params = useParams();
@@ -7,10 +8,37 @@ export default function CourseDetail({ database }) {
   const markup = database.map((database) => {
     if (database.id === paramsID) {
       return (
-        <div>
+        <div key={database.id}>
           <h1>{database.title}</h1>
           <p>CourseType: {database.courseType}</p>
           <img src={database.src} alt="" />
+          <ul key={database.id}>
+            Includes
+            {database.includes.map((includes) => {
+              return <li key={uuidv4()}>{includes}</li>;
+            })}
+          </ul>
+
+          <div className="learn">
+            <ul className="learn-list list-1">
+              {database.learnPartOne.map((learn) => {
+                return (
+                  <li className="learn-item" key={uuidv4()}>
+                    {learn}
+                  </li>
+                );
+              })}
+            </ul>
+            <ul className="learn-list list-2">
+              {database.learnPartTwo.map((learn) => {
+                return (
+                  <li className="learn-item" key={uuidv4()}>
+                    {learn}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       );
     }
