b485865d85398e10fffce5a0d10cd5470a9f170b
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-12 15:39:39 +0530

    removed database from main file & created a different file where all data is store

---
+++ src/components/data/database.js
@@ -0,0 +1,353 @@
+const database = [
+
+
+    {
+      src: "https://img-c.udemycdn.com/course/240x135/2153774_bef0_4.jpg",
+      title: "Modern HTML & CSS From The Beginning (Including Sass)",
+      id: 1,
+      courseType: "FE",
+      tutor: "Brad Traversy",
+      includes: [
+        "21 Hours on-demand video",
+        "2 articles",
+        "26 downloadable resources",
+        "Full lifetime access",
+        "Certificate of completion",
+      ],
+      learnPartOne: [
+        "Build High Quality Website & UI Projects",
+        "Flexbox & CSS Grid Projects",
+        "Website Hosting & Deployment With FTP & GIt",
+      ],
+      learnPartTwo: [
+        "HTML5 Semantic Layout & CSS Fundamentals",
+        "CSS Variables, Transition, Dropdowns, Overlays & More",
+        "All Skill Levels",
+      ],
+    },
+    {
+      src: "https://img-c.udemycdn.com/course/240x135/1463348_52a4_2.jpg",
+      title: "Modern JavaScript From The Beginning",
+      id: 2,
+      courseType: "FE",
+      tutor: "Brad Traversy",
+      includes: [
+        "21 Hours on-demand video",
+        "2 articles",
+        "26 downloadable resources",
+        "Full lifetime access",
+        "Certificate of completion",
+      ],
+      learnPartOne: [
+        "Modular learning sections & 10 real world projects with pure JavaScript",
+        "Asynchronous programming with Ajax, Fetch API, Promises & Async / Await",
+        "Learn JavaScript Patterns",
+      ],
+      learnPartTwo: [
+        "Master the DOM (document object model) WITHOUT jQuery",
+        "OOP including ES5 prototypes & ES2015 classes",
+        "Regular expressions, error handling, localStorage & more",
+      ],
+    },
+    {
+      src: "https://img-c.udemycdn.com/course/240x135/1362070_b9a1_2.jpg",
+      title: "React - The Complete Guide (incl Hooks, React Router, Redux)",
+      id: 3,
+      courseType: "FE",
+      tutor: "Maximilian Schwarzmüller",
+      includes: [
+        "48 Hours on-demand video",
+        "2 articles",
+        "31 downloadable resources",
+        "Full lifetime access",
+        "Certificate of completion",
+      ],
+      learnPartOne: [
+        "Build powerful, fast, user-friendly and reactive web apps",
+        "Apply for high-paid jobs or work as a freelancer in one the most-demanded sectors you can find in web dev right now",
+      ],
+      learnPartTwo: [
+        "Provide amazing user experiences by leveraging the power of JavaScript with ease",
+        "Learn all about React Hooks and React Components",
+      ],
+    },
+    {
+      src: "https://res.cloudinary.com/wesbos/image/fetch/w_700,q_auto,f_auto/https://courses.wesbos.com/images/JS3-social-share.png",
+      title: "JavaScript30",
+      id: 4,
+      courseType: "FE",
+      tutor: "Wesbos",
+      includes: [
+        "48 Hours on-demand video",
+        "2 articles",
+        "30 downloadable resources",
+        "Full lifetime access",
+      ],
+      learnPartOne: [
+        "Build powerful 30 projects with vanilla JS",
+        "Apply for high-paid jobs or work as a freelancer in one the most-demanded sectors you can find in web dev right now",
+      ],
+      learnPartTwo: [
+        "Learn various things like eventListeners, DOM, Higher Order Functions, etc",
+        "Lorem Ipsium",
+      ],
+    },
+    {
+      src: "https://img-c.udemycdn.com/course/240x135/4427730_5388.jpg",
+      title: "React Front To Back 2022",
+      id: 5,
+      courseType: "FE",
+      tutor: "Brad Traversy",
+      includes: [
+        "48 Hours on-demand video",
+        "2 articles",
+        "30 downloadable resources",
+        "Full lifetime access",
+      ],
+      learnPartOne: [
+        "Learn modern React by building 4 projects",
+        "Feedback app with in depth explanation of React fundamentals",
+        "Learn React hooks and how to create custom hooks",
+        "Build a Fullstack MERN support ticket system with Redux Toolkit",
+      ],
+      learnPartTwo: [
+        "Suitable For Both Beginners & Intermediate React Developers",
+        "Build a house marketplace with React and Firebase 9",
+        "Learn how to use context and reducers to manage global state",
+      ],
+    },
+    {
+      src: "https://img-c.udemycdn.com/course/240x135/995016_ebf4_3.jpg",
+      title: "Vue - The Complete Guide (incl. Router & Composition API)",
+      id: 6,
+      courseType: "FE",
+      tutor: "Maximilian Schwarzmüller",
+      includes: [
+        "48 Hours on-demand video",
+        "2 articles",
+        "31 downloadable resources",
+        "Full lifetime access",
+        "Certificate of completion",
+      ],
+      learnPartOne: [
+        "Build amazing Vue.js Applications - all the Way from Small and Simple Ones up to Large Enterprise-level Ones",
+        "Leverage Vue.js in both Multi- and Single-Page-Applications (MPAs and SPAs)",
+      ],
+      learnPartTwo: [
+        "Understand the Theory behind Vue.js and use it in Real Projects",
+        "Learn the latest version of Vue (Vue 3), including the brand-new Composition API",
+      ],
+    },
+    {
+      src: "https://img-c.udemycdn.com/course/240x135/1672410_9ff1_5.jpg",
+      title: "Node.js, Express, MongoDB & More: The Complete Bootcamp 2022",
+      id: 7,
+      courseType: "BE",
+      tutor: "Jonas Schmedtmann",
+      includes: [
+        "48 Hours on-demand video",
+        "2 articles",
+        "31 downloadable resources",
+        "Full lifetime access",
+        "Certificate of completion",
+      ],
+      learnPartOne: [
+        "Master the entire modern back-end stack: Node, Express, MongoDB and Mongoose (MongoDB JS driver)",
+        "Build a fast, scalable, feature-rich RESTful API (includes filters, sorts, pagination, and much more)",
+        "CRUD operations with MongoDB and Mongoose",
+        "How to work with data in NoSQL databases (including geospatial data)",
+        "Security: encryption, sanitization, rate limiting, etc.",
+        "Credit card payments with Stripe",
+        "Deploy the final application to production (including a Git crash-course)",
+      ],
+      learnPartTwo: [
+        "Build a complete, beautiful & real-world application from start to finish (API and server-side rendered website)",
+        "Learn how Node really works behind the scenes: event loop, blocking vs non-blocking code, streams, modules, etc.",
+        "Deep dive into mongoose (including all advanced features)",
+        "Advanced authentication and authorization (including password reset)",
+        "Server-side website rendering with Pug templates",
+        "Downloadable videos, code and design assets for projects",
+      ],
+    },
+    {
+      src: "https://img-c.udemycdn.com/course/240x135/1879018_95b6_3.jpg",
+      title: "NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)",
+      id: 8,
+      courseType: "BE",
+      tutor: "Maximilian Schwarzmüller",
+      includes: [
+        "48 Hours on-demand video",
+        "2 articles",
+        "31 downloadable resources",
+        "Full lifetime access",
+        "Certificate of completion",
+      ],
+      learnPartOne: [
+        "Work with one of the most in-demand web development programming languages",
+        "Build modern, fast and scalable server-side web applications with NodeJS, databases like SQL or MongoDB and more",
+        "Get a thorough introduction to DenoJS",
+      ],
+      learnPartTwo: [
+        "Learn the basics as well as advanced concepts of NodeJS in great detail",
+        "Understand the NodeJS ecosystem and build server-side rendered apps, REST APIs and GraphQL APIs",
+      ],
+    },
+    {
+      src: "https://img-c.udemycdn.com/course/240x135/684824_3626_3.jpg",
+      title: "Spring Framework Master Class - Java Spring the Modern Way",
+      id: 9,
+      courseType: "BE",
+      tutor: "in28Minutes Official",
+      includes: [
+        "48 Hours on-demand video",
+        "2 articles",
+        "31 downloadable resources",
+        "Full lifetime access",
+        "Certificate of completion",
+      ],
+      learnPartOne: [
+        "You will Learn Spring Framework the MODERN WAY - The way Real Projects use it!",
+        "You will Acquire ALL the SKILLS to demonstrate an EXPERTISE with Spring Framework in Your Job Interviews",
+        "You will Become the GO TO GUY for Fixing Spring Framework problems in Your Project",
+        "You will understand how to use Spring Annotations - @Autowired, @Component, @Service, @Repository, @Configuration, @Primary....",
+        "You will use a variety of Spring Boot Starters - Spring Boot Starter Web, Starter Data Jpa, Starter Test"
+      ],
+      learnPartTwo: [
+        "You will Become a COMPLETE Spring Developer - With the ability to write Great Unit Tests",
+        "You will learn the BASICS of Spring - Dependency Injection, IOC Container, Application Context and Bean Factory",
+        "You will GO FROM a Total Beginner to an EXPERIENCED Spring Developer",
+        "You will understand Spring MVC in depth - DispatcherServlet , Model, Controllers and ViewResolver",
+        "You will learn the basics of Spring Boot, Spring AOP, Spring JDBC and JPA",
+        "You will develop a basic Web application step by step using JSP Servlets and Spring MVC"
+      ],
+    },
+    {
+      src: "https://img-c.udemycdn.com/course/240x135/1699208_604f_3.jpg",
+      title: "The Flask Mega-Tutorial (Python Web Development)",
+      id: 10,
+      courseType: "BE",
+      tutor: "Miguel Grinberg",
+      includes: [
+        "48 Hours on-demand video",
+        "2 articles",
+        "31 downloadable resources",
+        "Full lifetime access",
+        "Certificate of completion",
+      ],
+      learnPartOne: [
+        "Write complete web applications using Python and Flask",
+        "Use relational databases",
+        "Translate and localize application messages",
+        "Add JavaScript user-friendly features",
+        "Deploy your application on Linux servers or Docker containers"
+      ],
+      learnPartTwo: [
+        "Accept user input through web forms",
+        "Manage user logins and authentication",
+        "Implement a search engine",
+        "Application Programming Interfaces (APIs)",
+      ],
+    },
+    {
+      src: "https://img-b.udemycdn.com/course/240x135/2640372_5b44_5.jpg?secure=wDfQpdLWRAfsMOZCAav5Dw%3D%3D%2C1644566660",
+      title: "React, NodeJS, Express & MongoDB - The MERN Fullstack Guide",
+      id: 11,
+      courseType: "FS",
+      tutor: "Maximilian Schwarzmüller",
+      includes: [
+        "48 Hours on-demand video",
+        "2 articles",
+        "31 downloadable resources",
+        "Full lifetime access",
+        "Certificate of completion",
+      ],
+      learnPartOne: [
+        "Learn how to connect ReactJS with NodeJS, Express & MongoDB",
+        "Refresh the basics about ReactJS, NodeJS, Express and MongoDB",
+        "Add File Upload to ReactJS + Node/ Express Applications",
+      ],
+      learnPartTwo: [
+        "Build an entire project from scratch!",
+        "Learn how to implement Authentication & Authorization",
+      ],
+    },
+    {
+      src: "https://img-c.udemycdn.com/course/240x135/3655840_1c3c.jpg",
+      title: "The Ultimate 2022 Fullstack Web Development Bootcamp",
+      id: 12,
+      courseType: "FS",
+      tutor: "Kane Ezki, Kalob Taulien, Radek Wilk ",
+      includes: [
+        "48 Hours on-demand video",
+        "2 articles",
+        "31 downloadable resources",
+        "Full lifetime access",
+        "Certificate of completion",
+      ],
+      learnPartOne: [
+        "Be able to build ANY website you want.",
+        "Master backend development with Django and Python",
+        "Craft several projects to show off to your future employer as a junior developer",
+        "Master frontend development with HTML, CSS and JavaScript",
+        "Learn professional developer best practices.",
+        "Learn how to use your command line tools like a boss",
+        "Learn how to make beautiful websites quickly with Bootstrap 5"
+      ],
+      learnPartTwo: [
+        "Build fully-fledged websites and web apps for your startup or business.",
+        "Learn the latest frameworks and technologies, including Javascript ES6+, Bootstrap 5, Django, Postgres and more.",
+        "Master backend development with Python, Django, Wagtail and Postgres",
+        "Learn modern workflow using Git and Github"
+      ],
+    },
+    {
+      src: "https://img-c.udemycdn.com/course/240x135/1646980_23f7_2.jpg",
+      title: "MERN Stack Front To Back: Full Stack React, Redux & Node.js",
+      id: 13,
+      courseType: "FS",
+      tutor: "Brad Traversy",
+      includes: [
+        "48 Hours on-demand video",
+        "2 articles",
+        "31 downloadable resources",
+        "Full lifetime access",
+        "Certificate of completion",
+      ],
+      learnPartOne: [
+        "Build a full stack social network app with React, Redux, Node, Express & MongoDB",
+        "Use Stateless JWT authentication practices",
+        "React Hooks, Async/Await & modern practices",
+        "Deploy to Heroku with a postbuild script"
+      ],
+      learnPartTwo: [
+        "Create an extensive backend API with Express",
+        "Integrate React with an Express backend in an elegant way",
+        "Use Redux for state management"
+      ],
+    },
+    {
+      src: "https://img-b.udemycdn.com/course/240x135/1042110_ffc3_4.jpg?secure=EkF9z2_XaXJoeaQGLaokSg%3D%3D%2C1644564995",
+      title: " Beginner Full Stack Web Development: HTML, CSS, React & Node",
+      id: 15,
+      courseType: "FS",
+      tutor: "Mark Wahlbeck",
+      includes: [
+        "48 Hours on-demand video",
+        "2 articles",
+        "31 downloadable resources",
+        "Full lifetime access",
+        "Certificate of completion",
+      ],
+      learnPartOne: [
+        "Build websites with HTML & CSS",
+        "Work with NoSQL databases like MongoDB",
+        "Build mobile-friendly websites with Bootstrap 4 & CSS",
+      ],
+      learnPartTwo: [
+        "Build backend servers & APIs with Node and Express",
+        "Build full-stack web apps with Facebook's React Framework",
+      ],
+    }
+  
+]
+export default database
--- src/index.js
+++ src/index.js
@@ -8,99 +8,7 @@ import FrontEnd from "./components/stack/FrontEnd";
 import BackEnd from "./components/stack/BackEnd";
 import FullStack from "./components/stack/FullStack";
 import CourseDetail from "./components/CourseDetail";
-
-const database = [
-  {
-    src: "https://img-c.udemycdn.com/course/240x135/2153774_bef0_4.jpg",
-    title: "Modern HTML & CSS From The Beginning (Including Sass)",
-    id: 1,
-    courseType: "FE",
-  },
-  {
-    src: "https://img-c.udemycdn.com/course/240x135/1463348_52a4_2.jpg",
-    title: "Modern JavaScript From The Beginning",
-    id: 2,
-    courseType: "FE",
-  },
-  {
-    src: "https://img-c.udemycdn.com/course/240x135/1362070_b9a1_2.jpg",
-    title: "React - The Complete Guide (incl Hooks, React Router, Redux)",
-    id: 3,
-    courseType: "FE",
-  },
-  {
-    src: "https://res.cloudinary.com/wesbos/image/fetch/w_700,q_auto,f_auto/https://courses.wesbos.com/images/JS3-social-share.png",
-    title: "JavaScript30",
-    id: 4,
-    courseType: "FE",
-  },
-  {
-    src: "https://img-c.udemycdn.com/course/240x135/4427730_5388.jpg",
-    title: "React Front To Back 2022",
-    id: 5,
-    courseType: "FE",
-  },
-  {
-    src: "https://img-c.udemycdn.com/course/240x135/995016_ebf4_3.jpg",
-    title: "Vue - The Complete Guide (incl. Router & Composition API)",
-    id: 6,
-    courseType: "FE",
-  },
-  {
-    src: "https://img-c.udemycdn.com/course/240x135/1672410_9ff1_5.jpg",
-    title: "Node.js, Express, MongoDB & More: The Complete Bootcamp 2022",
-    id: 7,
-    courseType: "BE",
-  },
-  {
-    src: "https://img-c.udemycdn.com/course/240x135/1879018_95b6_3.jpg",
-    title: "NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)",
-    id: 8,
-    courseType: "BE",
-  },
-  {
-    src: "https://img-c.udemycdn.com/course/240x135/684824_3626_3.jpg",
-    title: "Spring Framework Master Class - Java Spring the Modern Way",
-    id: 9,
-    courseType: "BE",
-  },
-  {
-    src: "https://img-c.udemycdn.com/course/240x135/1699208_604f_3.jpg",
-    title: "The Flask Mega-Tutorial (Python Web Development)",
-    id: 10,
-    courseType: "BE",
-  },
-  {
-    src: "https://img-b.udemycdn.com/course/240x135/2640372_5b44_5.jpg?secure=wDfQpdLWRAfsMOZCAav5Dw%3D%3D%2C1644566660",
-    title: "React, NodeJS, Express & MongoDB - The MERN Fullstack Guide",
-    id: 11,
-    courseType: "FS",
-  },
-  {
-    src: "https://img-c.udemycdn.com/course/240x135/3655840_1c3c.jpg",
-    title: "The Ultimate 2022 Fullstack Web Development Bootcamp",
-    id: 12,
-    courseType: "FS",
-  },
-  {
-    src: "https://img-c.udemycdn.com/course/240x135/1646980_23f7_2.jpg",
-    title: "  MERN Stack Front To Back: Full Stack React, Redux & Node.js",
-    id: 13,
-    courseType: "FS",
-  },
-  {
-    src: "https://img-c.udemycdn.com/course/240x135/822444_a6db.jpg",
-    title: "Python and Django Full Stack Web Developer Bootcamp",
-    id: 14,
-    courseType: "FS",
-  },
-  {
-    src: "https://img-b.udemycdn.com/course/240x135/1042110_ffc3_4.jpg?secure=EkF9z2_XaXJoeaQGLaokSg%3D%3D%2C1644564995",
-    title: " Beginner Full Stack Web Development: HTML, CSS, React & Node",
-    id: 15,
-    courseType: "FS",
-  },
-];
+import database from "./components/data/database";
 
 ReactDOM.render(
   <Router>
@@ -114,10 +22,6 @@ ReactDOM.render(
         path="/:type/:slug/:id/:type"
         element={<CourseDetail database={database} />}
       />
-      {/* <Route
-        path="/frontend/Modern-HTML-&-CSS-From-The-Beginning-(Including-Sass)"
-        element={<FrontEndCourse />}
-      /> */}
     </Routes>
   </Router>,
   document.getElementById("root")
