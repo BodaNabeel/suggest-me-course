db082e6bf1b87bc1354fa0bbbf76fd03da06fb05
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-14 15:47:00 +0530

    wrapped main area of app in div

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import { NavLink, Outlet } from "react-router-dom";
 function App() {
   const setClassNameBtn = (state) => {
-  
     return state ? "btn btn-active" : "btn btn-inActive";
   };
   return (
     <>
-      <header className="header">suggest me course</header>
-      <h1 className="heading">select your interest:</h1>
-      <NavLink
-        className={({ isActive }) => setClassNameBtn(isActive)}
-        to="/frontend"
-      >
-        FrontEnd
-      </NavLink>
-      <NavLink
-        className={({ isActive }) => setClassNameBtn(isActive)}
-        to="/backend"
-      >
-        BackEnd
-      </NavLink>
-      <NavLink
-        className={({ isActive }) => setClassNameBtn(isActive)}
-        to="/fullstack"
-      >
-        Full Stack
-      </NavLink>
+      <div className="main">
+        <header className="header">suggest me a course 👀</header>
+        <h3 className="heading">select your interest:</h3>
+        <div className="toggle-btn">
+          <NavLink
+            className={({ isActive }) => setClassNameBtn(isActive)}
+            to="/frontend"
+          >
+            FrontEnd
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => setClassNameBtn(isActive)}
+            to="/backend"
+          >
+            BackEnd
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => setClassNameBtn(isActive)}
+            to="/fullstack"
+          >
+            Full Stack
+          </NavLink>
+        </div>
+      </div>
 
       <div className="courses-container">
         <Outlet />
